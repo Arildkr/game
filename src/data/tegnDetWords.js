538a46a13d4a257ad016a933495da30494689905
Arildkr <arildkristensen@gmail.com>
2026-02-03 07:55:12 +0100

    Update tegnDetWords.js

--- src/data/tegnDetWords.js
+++ src/data/tegnDetWords.js
@@ -2,44 +2,54 @@
 // Ord som passer godt for tegning i klasserom
 
 const words = {
-  easy: [
+easy: [
     // Dyr
     'hund', 'katt', 'fisk', 'fugl', 'kanin', 'mus', 'elefant', 'løve',
     'giraff', 'ape', 'slange', 'frosk', 'skilpadde', 'ku', 'gris', 'sau',
+    'hest', 'and', 'kylling', 'ugle', 'bjørn', 'rev',
     // Mat
     'eple', 'banan', 'pizza', 'is', 'kake', 'hamburger', 'pølse', 'egg',
+    'gulrot', 'ost', 'brød', 'brus', 'jordbær', 'druer',
     // Gjenstander
     'bil', 'hus', 'tre', 'sol', 'måne', 'stjerne', 'blomst', 'ball',
     'bok', 'stol', 'bord', 'seng', 'dør', 'vindu', 'klokke', 'lampe',
+    'kopp', 'skje', 'kniv', 'gaffel', 'paraply', 'hatt', 'briller',
     // Kropp
-    'hånd', 'fot', 'øye', 'nese', 'munn', 'øre', 'hjerte',
+    'hånd', 'fot', 'øye', 'nese', 'munn', 'øre', 'hjerte', 'hår',
   ],
   medium: [
     // Aktiviteter
     'svømme', 'løpe', 'sove', 'spise', 'lese', 'skrive', 'tegne', 'synge',
-    'danse', 'hoppe', 'klatre', 'sykle', 'kjøre bil',
+    'danse', 'hoppe', 'klatre', 'sykle', 'kjøre bil', 'fiske', 'børste tenner',
+    'vaske hender', 'gå på ski', 'spille gitar', 'sparke fotball',
     // Steder
     'skole', 'sykehus', 'butikk', 'restaurant', 'kirke', 'slott', 'strand',
-    'fjell', 'skog', 'innsjø',
+    'fjell', 'skog', 'innsjø', 'park', 'kino', 'bondegård', 'flyplass', 'tivoli',
     // Mer avanserte gjenstander
     'paraply', 'briller', 'sko', 'hatt', 'veske', 'nøkkel', 'telefon',
-    'datamaskin', 'fjernkontroll', 'saks', 'hammer', 'stige',
+    'datamaskin', 'fjernkontroll', 'saks', 'hammer', 'stige', 'tannbørste',
+    'ryggsekk', 'koffert', 'speil', 'lommelykt', 'kompass', 'støvsuger',
     // Dyr
     'pingvin', 'hai', 'hval', 'blekksprut', 'sommerfugl', 'bie', 'edderkopp',
-    'snegl', 'krokodille', 'kamel', 'isbjørn',
+    'snegl', 'krokodille', 'kamel', 'isbjørn', 'flaggermus', 'ekorn', 'piggsvin',
   ],
   hard: [
-    // Abstrakte konsepter (tegnes gjennom handling)
-    'drøm', 'latter', 'vennskap', 'sjalusi', 'forelsket', 'redd',
+    // Abstrakte konsepter
+    'drøm', 'latter', 'vennskap', 'sjalusi', 'forelsket', 'redd', 'sint',
+    'tålmodighet', 'flaks', 'hemmelighet', 'tid', 'musikk', 'duft',
     // Komplekse handlinger
-    'fotografere', 'jogge', 'meditere', 'surfe', 'snowboard',
+    'fotografere', 'jogge', 'meditere', 'surfe', 'snowboard', 'grille',
+    'strikke', 'trylle', 'undersøke', 'feire', 'overraske',
     // Yrker
     'lege', 'brannmann', 'politi', 'kokk', 'lærer', 'pilot', 'astronaut',
+    'bonde', 'frisør', 'kunstner', 'snekker', 'tannlege', 'forsker',
     // Komplekse objekter
     'romskip', 'vulkan', 'tornado', 'regnbue', 'vannfall', 'pyramide',
     'skateboard', 'helikopter', 'ubåt', 'traktor', 'motorsykkel',
+    'mikroskop', 'kikkert', 'statue', 'anker', 'rulleskøyter',
     // Eventyr/fantasi
     'drage', 'enhjørning', 'trollmann', 'havfrue', 'vampyr', 'robot',
+    'spøkelse', 'sjørøver', 'romvesen', 'fe', 'troll', 'superhelt',
   ]
 };
 
